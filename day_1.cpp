034bc4892bbecfecae39859b1663a2dfe37fed73
adarshvi171 <av2459935@gmail.com>
2026-08-02 16:02:15 +0530

    c++ practice till for loop

---
+++ day_1.cpp
@@ -0,0 +1,70 @@
+//  #include<iostream>
+// using namespace std;
+// int main() {
+// cout<<"my name is \n" <<"adarsh \n"<<"vishwakarama";
+// return 0;
+// }
+
+// variables term 
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// int main() {
+//     int age =18;
+//     string name ="adarsh";
+// cout << name << " " << age << endl;
+// return 0;
+// }
+
+
+//  Type casting in c++
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// double price =100.99;
+// int newprice =(int) price;
+// cout << newprice << endl;
+// return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//  int age ;
+//  string name ;
+//  cout << "enter the name :"<<endl;
+//  cin >> name;
+//  cout << "my name is :" <<name << endl;
+// return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+// float a =45,b=34;
+// cout << "your mult ="<< (a/b) << endl;
+// return 0;
+// }
+
+// operators 
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     cout << (9<=9)<< endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int a , b ;
+//     cout << "enter number 1="<< endl;
+//     cin >> a;
+//     cout << "enter number 2 ="<< endl;
+//     cin >> b;
+//     cout << "answer ="<< (a+b)<< endl;
+// return 0;
+// }
---
+++ day_2.cpp
@@ -0,0 +1,92 @@
+// #include<iostream>
+// using namespace std;
+// int main (){
+// int a= 45;
+// if (a>=87){
+//     cout <<"a is possitive number"<<endl;
+// }
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std ;
+// int main (){
+// int a =45;
+// if (a>=90){
+//     cout << "a is positive number"<<endl;
+// }else{
+//     cout << "invalid"<<endl;
+// }
+//     return 0;
+// }
+
+// vote system
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+// int age;
+// cout <<"enter the age : "<<endl;
+// cin >> age ;
+// if (age >=18){
+//     cout << "person can vote"<< endl;
+// }else{
+//     cout <<"person can not vote"<< endl;
+// }
+//     return 0;
+// }
+
+//odd or even number 
+
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+// int num ;
+// cout <<"number is :"<< endl;
+// cin >>num;
+// if (num%2 ==0){
+//     cout << "number is even"<<endl;
+// }else{
+//     cout <<"number is odd"<<endl;
+// }
+//     return 0;
+// }
+
+// while loop 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n=20;    
+// int count= 5;
+// while (count <=n){
+//     cout << count << " "<< endl;
+//     count ++;
+// }
+// cout <<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n=30;
+// for(int i=3 ; i <=n; i= i+3){
+//     cout <<i << endl;
+// }
+// cout <<endl;
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int main (){
+int n =3;
+int sum= 0;
+for(int i=1; i<=n; i++){
+    sum += i;
+}
+cout<<sum <<endl;
+    return 0;
+}
