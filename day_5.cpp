8d40175d5fc9fa02ff8b7a4b28a9238d0a20e6ba
adarshvi171 <av2459935@gmail.com>
2026-08-24 17:45:52 +0530

    Array....

--- day_5.cpp
+++ day_5.cpp
@@ -40,10 +40,109 @@
 
 //          bitwise 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<< sizeof(int)<< endl;
+//    cout<< sizeof(long long int)<< endl;
+//     return 0;
+// }
+
+ 
+
+//...........arrays
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int marks[5]={45,87,78,90,58};
+//     int size =5;
+//     for(int i=0; i<size; i++){
+//         cout<<marks[i]<<endl;
+//     }
+//     return 0;
+// }
+
+
+// finding smallest no. in array
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  int num[]={1299,5546,2554,84658,5654,4325};
+//  int size = 6;
+//  int min= INT_MAX;
+//  for(int i=0; i<size; i++){
+//     if(num[i]<min){
+//         min=num[i];
+//     }
+//  }
+//  cout<<min<<endl;
+// return 0;
+// }
+
+
+// linear search 
+
 #include<iostream>
 using namespace std;
+int linearsearch(int arr[],int sz,int target){
+    for(int i=0; i<sz; i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
 int main(){
-    cout<< sizeof(int)<< endl;
-   cout<< sizeof(long long int)<< endl;
+int arr[]={4,2,7,8,1,2,5};
+int sz=7;
+int target=54;
+cout<<linearsearch(arr,sz,target)<<endl;
     return 0;
 }
+
+// reverse arry
+
+#include<iostream>
+using namespace std;
+void reversearry(int arr[],int sz){
+    int start=0,end=sz-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+int main(){
+int arr[]={4,2,7,8,1,2,5};
+int sz=7;
+reversearry(arr,sz);
+for(int i=0; i<sz; i++){
+    cout<<arr[i]<< " ";
+}
+cout<<endl;
+    return 0;
+}
+
+
+                    // HOME WORK QUESTIIOIONS
+
+//a function adding arrys number
+
+#include<iostream>
+using namespace std;
+int sum(int arr[],int sz){
+    int num=0;
+    for(int i=0;i<sz;i++){
+        num+=arr[i];  
+    }
+    return num;
+}
+int main (){
+int arr[]={1,1,1,1,1,1,1};
+int sz=7;
+cout<< sum(arr,sz)<< endl;
+    return 0;
+}
+
