2842d95a400b008bb97f0d1b80f29848b9b04d1f
adarshvi171 <av2459935@gmail.com>
2026-09-05 09:58:41 +0530

    pattern and loop

--- day_3.cpp
+++ day_3.cpp
@@ -208,76 +208,76 @@
 // }
 
 
-// #include<iostream>
-// using namespace std;
-// int main (){
-// int n=4;
-// for (int i=0; i<n; i++){
-//     for(int j=0; j<i; j++){
-//         cout<< " ";
-//     }
-//     for(int j=0; j<n-i; j++){
-//         cout<<i+1;
-//     }
-//     cout<< endl;
-// }
-//     return 0;
-// }
+#include<iostream>
+using namespace std;
+int main (){
+int n=4;
+for (int i=0; i<n; i++){
+    for(int j=0; j<i; j++){
+        cout<< " ";
+    }
+    for(int j=0; j<n-i; j++){
+        cout<<i+1;
+    }
+    cout<< endl;
+}
+    return 0;
+}
 
-// pyramid tringle
+pyramid tringle
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-// int n=4;
-// for(int i=0;i<n; i++){
-//     for (int j=0; j<n-i-1; j++){
-//         cout<< " ";
-//     }
-//     for(int j=1; j<=i+1; j++){
-//         cout<< j;   
-//     }
-//     for (int j=i; j>0; j--){
-//         cout<< j;
-//     }
-//     cout <<endl;
-// }
-//     return 0;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+int n=4;
+for(int i=0;i<n; i++){
+    for (int j=0; j<n-i-1; j++){
+        cout<< " ";
+    }
+    for(int j=1; j<=i+1; j++){
+        cout<< j;   
+    }
+    for (int j=i; j>0; j--){
+        cout<< j;
+    }
+    cout <<endl;
+}
+    return 0;
+}
 
 
-// butterfly pattern
+butterfly pattern
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-// int n=4;
-// for (int i=0; i<n; i++){
+#include<iostream>
+using namespace std;
+int main(){
+int n=4;
+for (int i=0; i<n; i++){
 
-//     for(int j=0;j<i+1; j++){
-//         cout<< "*";
-//     }
-//     for(int j=0; j<(2+n)-(i+i); j++){
-//         cout<< " ";
-//     }
-//     for(int j=0; j<i+1; j++){
-//         cout<< "*";
-//     }
-//     cout<<endl;
-// }
-// for(int i=0; i<n; i++){
-//     for (int j=0; j<n-i; j++){
-//         cout<<"*";
-//     }
-//     for (int j=0; j<(n-4)+(i+i); j++){
-//         cout<< " ";
-//     }
-//     for( int j=0; j<n-i;j++){
-//         cout<< "*";
-//     }
-//     cout<<endl;
-// }
+    for(int j=0;j<i+1; j++){
+        cout<< "*";
+    }
+    for(int j=0; j<(2+n)-(i+i); j++){
+        cout<< " ";
+    }
+    for(int j=0; j<i+1; j++){
+        cout<< "*";
+    }
+    cout<<endl;
+}
+for(int i=0; i<n; i++){
+    for (int j=0; j<n-i; j++){
+        cout<<"*";
+    }
+    for (int j=0; j<(n-4)+(i+i); j++){
+        cout<< " ";
+    }
+    for( int j=0; j<n-i;j++){
+        cout<< "*";
+    }
+    cout<<endl;
+}
 
-//     return 0;
-// }
+    return 0;
+}
 
